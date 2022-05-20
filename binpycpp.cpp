0dbe363d13b6d334b7e33fba41e5d0ddd0615ea3
xingyujie <xingyujie50@gmail.com>
2022-05-21 00:31:14 +0800

    Create binpycpp.cpp

---
+++ binpycpp.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int binpy(int filename);
+{
+int getfile;
+int result = system("binpython.exe -f strcat(getfile,t)");
+return result;
+}
+int main()
+{
+    binpy(xxxx.py);
+}
